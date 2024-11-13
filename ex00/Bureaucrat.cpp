4b8b34695c79f17c9b29682288e0cbfdc2155e65
EV <144735628+EfrainVisconti@users.noreply.github.com>
2024-11-13 16:52:38 +0000

    Bureaucrat class implementation

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:06:53 by codespace         #+#    #+#             */
-/*   Updated: 2024/11/13 15:46:28 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/13 16:51:31 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,42 @@
 Bureaucrat::Bureaucrat() : _name("default"), _grade(1)
 {
     std::cout << "Default Bureaucrat constructor called" << std::endl;
+    //to do throw error
 }
 
 Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name), _grade(grade)
 {
-    std::cout << "Bureaucrat constructor called: " << 
+    std::cout << "Bureaucrat constructor called: " << this->_name << std::endl;
+    //to do throw error
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+{
+    std::cout << "Bureaucrat copy constructor called" << std::endl;
+    *this = other;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat destructor called: " << this->_name << std::endl;
+}
+
+Bureaucrat  &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    std::cout << "Bureaucrat copy assignment operator called" << std::endl;
+    if (this == &other)
+        return *this;
+    this->_name = other.getName();
+    this->_grade = other.getGrade();
+    return *this;
+}
+
+std::string Bureaucrat::getName()
+{
+    return this->_name;
+}
+
+int Bureaucrat::getGrade()
+{
+    return this->_grade;
 }
